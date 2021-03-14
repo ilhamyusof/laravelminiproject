bf909e128b10d97d7111da558d1e6f38586b4770
ilhamyusof <41537105+ilhamyusof@users.noreply.github.com>
2021-03-14 11:28:43 +0800

    Added REST Api

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Welcome tp Ilham Insta
+
+This is my side project email template. Thank you if you read this email
+
+@component('mail::button', ['url' => ''])
+Button Text
+@endcomponent
+
+Thanks,<br>
+Ilham
+@endcomponent
